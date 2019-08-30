caea12ac2ac7b15136c574d7724c8d1fbde5e9d1
Oscar <scardario@gmail.com>
2019-08-29 23:40:04 -0500

    models created  and __str__ methods added

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,29 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Pregunta(models.Model):
+    enunciado_pregunta = models.CharField(max_length=200)
+
+    fecha_pub = models.DateTimeField('fecha de publicación')
+
+    def __str__(self):
+        return str(self.id) + " - " + str(self.enunciado_pregunta)
+        #return str(self.enunciado_pregunta)
+
+    def fue_publicada_recientemente(self):
+        return self.fecha_pub >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Opcion(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+    texto_opcion = models.CharField(max_length=200)
+
+    votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.texto_opcion
