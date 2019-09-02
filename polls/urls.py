976c9ef3ea92128118a0fb8e9c12ccb7566a3afd
Oscar <scardario@gmail.com>
2019-09-02 00:23:38 -0500

    added template files

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    #/polls/
     path('', views.index, name='index'),
+    # /polls/5/
+    path('<int:pregunta_id>/', views.detalle, name='detalle'),
+    # /polls/5/resultados/
+    path('<int:pregunta_id>/resultados/', views.resultados, name='resultados'),
+    # /polls/5/votacion/
+    path('<int:pregunta_id>/votacion/', views.votacion, name='votacion'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
+#Borrar estos dos cuando se cambien todos los métodos para que usen render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Pregunta
 
 
 def index(request):
-    return HttpResponse("Hey, you're at the polls page")
+    lista_preguntas_actualizada = Pregunta.objects.order_by('-fecha_pub')[:5]
+    contexto = {
+        'lista_preguntas_actualizada': lista_preguntas_actualizada,
+    }
+    return render(request, 'polls/index.html', contexto)
+
+def detalle(request, pregunta_id):
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+    return render(request, "polls/detalle.html", {'pregunta': pregunta})
+
+def resultados(request, pregunta_id):
+    respuesta = "Está consultando los resultados de la pregunta %s."
+    return HttpResponse(respuesta % pregunta_id)
+
+def votacion(request, pregunta_id):
+    return HttpResponse("Está votando en la pregunta %s." % pregunta_id)
