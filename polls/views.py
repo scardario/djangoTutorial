4b38c9a84d7bcab9e97425972a2cad822931ced5
Oscar <scardario@gmail.com>
2019-09-04 23:04:09 -0500

    added voting view, along with voting template

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,13 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
-#Borrar estos dos cuando se cambien todos los métodos para que usen render
-from django.http import HttpResponse
+#Borrar httpresponse y loader cuando se cambien todos los métodos para que usen render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
-from .models import Pregunta
+from django.urls import reverse
+
+from .models import Pregunta, Opcion
 
 
 def index(request):
@@ -24,4 +26,19 @@ def resultados(request, pregunta_id):
     return HttpResponse(respuesta % pregunta_id)
 
 def votacion(request, pregunta_id):
-    return HttpResponse("Está votando en la pregunta %s." % pregunta_id)
+
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+
+    try:
+        opcion_seleccionada = pregunta.opcion_set.get(pk=request.POST['opcion'])
+    except (KeyError, Pregunta.DoesNotExist):
+
+        #vuelve a cargar la pregunta si hay un error asignando la opción
+        return render(request, 'polls/detalle.html', {
+            'pregunta': pregunta,
+            'mensaje_error': "No se ha seleccionado ninguna opción",
+        })
+    else:
+        opcion_seleccionada.votos +=1
+        opcion_seleccionada.save()
+        return HttpResponseRedirect(reverse('polls:resultados', args=(pregunta.id,)))
